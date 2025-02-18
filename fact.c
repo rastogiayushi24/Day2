a3c94384d6fd03572df13606e4a580c5b246f078
rastogiayushi24 <4mt23ai012@mite.ac.in>
2025-02-18 14:48:59 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+long factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        long fact = factorial(num);
+        printf("Factorial of %d is %ld\n", num, fact);
+    }
+
+    return 0;
+}
